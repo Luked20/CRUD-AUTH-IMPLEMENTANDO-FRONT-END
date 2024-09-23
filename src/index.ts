4e2fa1c5aa3dc9752e9955cface3396f762d93ca
Lucas <89111514+Luked20@users.noreply.github.com>
2024-09-23 19:54:50 -0300

    Add files via upload

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import express from 'express';
+import http from 'http';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import router from './router';
+
+//Initiate our app
+//create an instance of the Express.js application
+const app = express();
+
+//Middleware
+//Auth
+app.use(cors({
+    credentials: true, //indicates that the server can include credentials (e.g., cookies, HTTP authentication) in CORS requests.
+}));
+app.use(compression()); //reduces the size of responses and improves the speed of transferring data over the network
+app.use(cookieParser()); //parses cookies from incoming requests
+app.use(bodyParser.json()); //used to parse incoming JSON payloads of HTTP POST requests. It extracts the JSON data from the request body and makes it accessible via req.body in your route handlers
+
+//Create server & listener
+const server = http.createServer(app);
+server.listen(3000, () => {
+    console.log('Server running on http://localhost:3000/');
+});
+
+//DB
+const MONGO_URL = 'mongodb+srv://lucasbissi2006iTgiso105bK4C2fI:iTgiso105bK4C2fI@cluster1.xbqty.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1';
+mongoose.Promise = Promise; //Promises are a programming pattern, handling asynchronous operations to avoid the "callback hell" problem.
+mongoose.connect(MONGO_URL);
+mongoose.connection.on('error', (error: Error) => console.log(error));
+
+app.use('/', router());
+
